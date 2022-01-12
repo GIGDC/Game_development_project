8ea83666706a4cc7fdf5873ddc62f49613ddcbea
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2022-01-12 16:04:20 +0900

    Feature/#2 character move (#3)
    * 플레이어 움직임 구현 (#2)
    
    * 입력받은 키에 따라 플레이어 이동 속도 변경  (#2)
    
    * 타일 sprite 수정

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    public float speed;
+    public float normalSpeed, crawlSpeed, runSpeed; 
+    Vector2 movement;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == 1;
+
+        // 이동에 따른 애니메이션
+        animator.SetInteger("WalkHorizontally", (int)movement.x);
+        animator.SetInteger("WalkVertically", (int)movement.y);
+
+        ChangeMoveSpeed();
+    }
+
+    void FixedUpdate()
+    {
+        // 이동
+        rigid.MovePosition(rigid.position + movement * speed * Time.deltaTime);
+    }
+
+    // 키 입력에 따른 이동 속도 변경
+    void ChangeMoveSpeed()
+    {
+        // 기어가기
+        if (Input.GetKey(KeyCode.C))
+        {
+            speed = crawlSpeed;
+            animator.SetFloat("MoveSpeed", 0.5f);
+        }
+        // 뛰기
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+            animator.SetFloat("MoveSpeed", 2f);
+        }
+        // 일반 속도
+        else
+        {
+            speed = normalSpeed;
+            animator.SetFloat("MoveSpeed", 1f);
+        }
+    }
+}
