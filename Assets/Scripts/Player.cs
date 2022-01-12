ff477205170404ab0f04031208298317bd6182ee
Sarah Lee <2234sarah@gmail.com>
2022-01-12 15:14:38 +0900

    입력받은 키에 따라 플레이어 이동 속도 변경  (#2)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,8 @@ public class Player : MonoBehaviour
     Animator animator;
     SpriteRenderer spriteRenderer;
 
-    public int moveSpeed;
+    public float speed;
+    public float normalSpeed, crawlSpeed, runSpeed; 
     Vector2 movement;
 
     private void Awake()
@@ -32,12 +33,39 @@ public class Player : MonoBehaviour
         if (Input.GetButton("Horizontal"))
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == 1;
 
+        // 이동에 따른 애니메이션
         animator.SetInteger("WalkHorizontally", (int)movement.x);
         animator.SetInteger("WalkVertically", (int)movement.y);
+
+        ChangeMoveSpeed();
     }
 
     void FixedUpdate()
     {
-        rigid.MovePosition(rigid.position + movement * moveSpeed * Time.deltaTime);
+        // 이동
+        rigid.MovePosition(rigid.position + movement * speed * Time.deltaTime);
+    }
+
+    // 키 입력에 따른 이동 속도 변경
+    void ChangeMoveSpeed()
+    {
+        // 기어가기
+        if (Input.GetKey(KeyCode.C))
+        {
+            speed = crawlSpeed;
+            animator.SetFloat("MoveSpeed", 0.5f);
+        }
+        // 뛰기
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+            animator.SetFloat("MoveSpeed", 2f);
+        }
+        // 일반 속도
+        else
+        {
+            speed = normalSpeed;
+            animator.SetFloat("MoveSpeed", 1f);
+        }
     }
 }
