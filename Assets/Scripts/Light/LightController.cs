6d1d9510799fae48c0a56c4077c0290d4825391a
OhJiwoo <jiu3159@naver.com>
2022-02-06 23:10:52 +0900

    조명 사진 변경 및 투명도 조절

--- Assets/Scripts/Light/LightController.cs
+++ Assets/Scripts/Light/LightController.cs
@@ -19,25 +19,41 @@ public class LightController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = thePlayer.transform.position;
+        Vector3 temp;
+        temp.x = 0.0F;
+        temp.y = 0.8F;
+        temp.z = 0.0F;
+        this.transform.position = thePlayer.transform.position - temp;
 
         vector.Set(thePlayer.direction.x, thePlayer.direction.y);
-        if(vector.x == 1f)
+        if (vector.x == 1f)
         {
             rotation = Quaternion.Euler(0, 0, 90);
             this.transform.rotation = rotation;
+            temp.x = -1.0F;
+            temp.y = -0.5F;
+            temp.z = 0.0F;
+            this.transform.position = thePlayer.transform.position - temp;
         }
-        else if(vector.x == -1f)
+        else if (vector.x == -1f)
         {
             rotation = Quaternion.Euler(0, 0, -90);
             this.transform.rotation = rotation;
+            temp.x = 1.0F;
+            temp.y = -0.5F;
+            temp.z = 0.0F;
+            this.transform.position = thePlayer.transform.position - temp;
         }
-        else if(vector.y == 1f)
+        else if (vector.y == 1f)
         {
             rotation = Quaternion.Euler(0, 0, 180);
             this.transform.rotation = rotation;
+            temp.x = 0.0F;
+            temp.y = -1.0F;
+            temp.z = 0.0F;
+            this.transform.position = thePlayer.transform.position - temp;
         }
-        else if(vector.y == -1f)
+        else if (vector.y == -1f)
         {
             rotation = Quaternion.Euler(0, 0, 0);
             this.transform.rotation = rotation;
