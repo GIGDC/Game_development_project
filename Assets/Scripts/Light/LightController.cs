fda0a71c7c54a319c0ce657ade96e6ca2ee0d2d2
OhJiwoo <jiu3159@naver.com>
2022-02-12 00:31:26 +0900

    조명 효과 구현 완료(#20)

--- Assets/Scripts/Light/LightController.cs
+++ Assets/Scripts/Light/LightController.cs
@@ -13,7 +13,6 @@ public class LightController : MonoBehaviour
 
     public GameObject monster;
     SpriteRenderer sr;
-    public bool trigerState;
 
     // Start is called before the first frame update
     void Start()
@@ -28,45 +27,8 @@ public class LightController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 temp;
-        temp.x = 0.0F;
-        temp.y = -0.5F;
-        temp.z = 0.0F;
-        this.transform.position = thePlayer.transform.position - temp;
-
+        this.transform.position = thePlayer.transform.position;
         vector.Set(thePlayer.direction.x, thePlayer.direction.y);
-        if (vector.x == 1f)
-        {
-            rotation = Quaternion.Euler(0, 0, 90);
-            this.transform.rotation = rotation;
-            temp.x = -0.4F;
-            temp.y = -0.3F;
-            temp.z = 0.0F;
-            this.transform.position = thePlayer.transform.position - temp;
-        }
-        else if (vector.x == -1f)
-        {
-            rotation = Quaternion.Euler(0, 0, -90);
-            this.transform.rotation = rotation;
-            temp.x = 0.3F;
-            temp.y = -0.5F;
-            temp.z = 0.0F;
-            this.transform.position = thePlayer.transform.position - temp;
-        }
-        else if (vector.y == 1f)
-        {
-            rotation = Quaternion.Euler(0, 0, 180);
-            this.transform.rotation = rotation;
-            temp.x = 0.0F;
-            temp.y = 0.0F;
-            temp.z = 0.0F;
-            this.transform.position = thePlayer.transform.position - temp;
-        }
-        else if (vector.y == -1f)
-        {
-            rotation = Quaternion.Euler(0, 0, 0);
-            this.transform.rotation = rotation;
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
