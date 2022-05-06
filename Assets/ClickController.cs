8add6e74001fba08c87637c60552c530c4b02c39
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2022-05-06 20:38:36 +0900

    마우스 위에 돋보기 띄우게 하기

--- Assets/ClickController.cs
+++ Assets/ClickController.cs
@@ -2,53 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class ClickController : MonoBehaviour
 {
-        public Image EventImage;
-        GameObject target;
-        public GameObject glass;
-        bool isCollider = false;
+    public Image EventImage;
+    GameObject target;
+    public GameObject glass;
+    bool isCollider = false;
 
-        private void FixedUpdate()
+    private void FixedUpdate()
+    {
+        CastRay();
+        if (isCollider)
         {
-            CastRay();
-            if (isCollider)
-            {
-                Vector3 position = glass.transform.localPosition;
-                position.x = target.transform.localPosition.x;
-                position.y = target.transform.localPosition.y;
-                glass.transform.localPosition = position;
+            glass.SetActive(true);
+            glass.transform.position = Input.mousePosition;
 
-                print(position);
-                glass.SetActive(true);
-                Debug.Log(target);
-                isCollider = false;
-            }
-            else
-            {
-                glass.SetActive(false);
-            }
-
-            if (Input.GetMouseButtonDown(0))
-            {
-                if (target == this.gameObject)
-                {
-                    EventImage.gameObject.SetActive(true);
-                    GameObject.Find("GameObject").GetComponentInChildren<Transform>().gameObject.SetActive(false);
-                    }
-            }
+            Debug.Log(target);
+            isCollider = false;
+        }
+        else
+        {
+            glass.SetActive(false);
         }
 
-        public void CastRay()
+        if (Input.GetMouseButtonDown(0))
         {
-            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, 0f);
-
-            if (hit.collider != null)
+            if (target == this.gameObject)
             {
-                target = hit.collider.gameObject;
-                isCollider = true;
-              
+                EventImage.gameObject.SetActive(true);
+                GameObject.Find("GameObject").GetComponentInChildren<Transform>().gameObject.SetActive(false);
             }
         }
     }
+
+    public void CastRay()
+    {
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, 0f);
+
+        if (hit.collider != null)
+        {
+            target = hit.collider.gameObject;
+            isCollider = true;
+              
+        }
+    }
+}
